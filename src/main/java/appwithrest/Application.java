dfcfa265570b5e706510e7c574803b94bf33a825
Oliver Gierke <info@olivergierke.de>
2014-07-25 17:10:35 +0200

    Fixed HttpMessageConverter setup.
    Moved to improved versions of Spring HATEOAS and Spring Data REST. Manually set up an HttpMessageConverters instance to be able to deactivate the HttpMessageConvertersAutoConfiguration (Web MVC auto-config requires that bean).
    
    I've filed [0] as I think the HttpMessageConverters instance in Boot shouldn't simply replace converters but add the ones found.
    
    [0] https://github.com/spring-projects/spring-boot/issues/1293

--- src/main/java/appwithrest/Application.java
+++ src/main/java/appwithrest/Application.java
@@ -2,14 +2,22 @@ package appwithrest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.web.HttpMessageConverters;
+import org.springframework.boot.autoconfigure.web.HttpMessageConvertersAutoConfiguration;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan
-@EnableAutoConfiguration
+@EnableAutoConfiguration(exclude = HttpMessageConvertersAutoConfiguration.class)
 public class Application {
+
+	@Bean
+	HttpMessageConverters httpMessageConverters() {
+		return new HttpMessageConverters();
+	}
 
 	public static void main(String[] args) {
 		ApplicationContext ctx = SpringApplication.run(Application.class, args);
